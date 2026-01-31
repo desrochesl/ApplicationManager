0b381b0852e00db9ec2811ea3a5160895b33bd1a
Luna <luna@desroches.org>
2026-01-30 21:26:11 -0500

    create applications table

--- src/dbManagement/dbmanagement.py
+++ src/dbManagement/dbmanagement.py
@@ -1,7 +1,32 @@
 import sqlite3 as sql
 
-def create_connection():
-    ...
+conn = sql.connect("applications.db")
 
-conn = sql.connect('applications.db')
+c = conn.cursor()
+
+# c.execute("""CREATE TABLE applications (
+#           companyName text,
+#           contact text,
+#           position text)""")
+        #   ,deadline DATE,
+        #   sentResume DATE,
+        #   recieved DATE,
+        #   interview DATE,
+        #   followUp text,
+        #   description text
+        #   )""")
+
+c.execute("DELETE FROM applications")
+
+
+c.execute("INSERT INTO applications VALUES ('Apple', 'My Mom', 'Nerd')")
+c.execute("INSERT INTO applications VALUES ('Google', 'My Mom', 'Senior Engineer')")
+
+c.execute("SELECT * FROM applications")
+
+print(c.fetchall())
+
+conn.commit()
+
+conn.close()
 

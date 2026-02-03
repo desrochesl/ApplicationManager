bc4b39610636f8d318c11760e58aa8b96283238c
Luna <luna@desroches.org>
2026-02-03 10:11:55 -0500

    Implement createDB function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+from dbManagement.dbmanagement import DBManager
+
 if __name__ == "__main__":
     print("Hello, World!")
-    ...
 
-    
+
+def createDB():
+    DB = DBManager("applications")
+    DB.create_table()
+    return DB
